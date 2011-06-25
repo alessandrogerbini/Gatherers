8a49951520771d1a02cc824ab4974aad7b059716
Alec Perkins <alecperkins@gmail.com>
2011-06-24 21:59:59 -0400

    Store locations.

--- content/js/store_locations.js
+++ content/js/store_locations.js
@@ -1,10 +1,60 @@
 
 window.store_locations = [
   {
+      name      : "Albany Honest Weight Food Coop",
+      address   : "484 Central Ave., Albany, NY 12206",
+      website   : "http://www.hwfc.com",
+      latitude  : 42.668565,
+      longitude : -73.778976
+  },
+  {
+      name      : "Niskayuna Consumers Cooperative",
+      address   : "2227 Nott St., Schenectady, NY 12309",
+      website   : "http://www.niskayunacoop.com",
+      latitude  : 42.815673,
+      longitude : -73.893515
+  },
+  {
       name      : "Pioneer Food Market",
-      address   : "77 Congress Street, Troy, New York 12180",
+      address   : "77-81 Congress St., Troy, New York 12180",
       website   : "http://www.troyfoodcoop.com",
       latitude  : 42.728752,
       longitude : -73.69043
   },
+  {
+      name      : "Four Seasons Natural Foods",
+      address   : "33 Phila St., Saratoga Springs, NY 12866",
+      website   : "http://fourseasonsnaturalfoods.com",
+      latitude  : 43.080353,
+      longitude : -73.784126
+  },
+  {
+      name      : "Organica",
+      address   : "296 Delaware Ave., Albany, NY 12209",
+      website   : "http://www.organicanewyork.com",
+      latitude  : 42.642944,
+      longitude : -73.776857
+  },
+  {
+      name      : "Schenectady Greenmarket",
+      address   : "Jay St., Schenectady, NY 12305",
+      website   : "http://schenectadygreenmarket.org",
+      latitude  : 42.814093,
+      longitude : -73.940024
+  },
+  {
+      name      : "Delmar Farmers Market",
+      address   : "332 Kenwood Ave, Delmar, NY 12054",
+      website   : "http://www.delmarmarket.org",
+      latitude  : 42.620774,
+      longitude : -73.824957
+  },
+  {
+      name      : "Capital District Farmers Market In Menands",
+      address   : "381 Broadway, Menands, NY 12204",
+      website   : "http://www.capitaldistrictfarmersmarket.org",
+      latitude  : 42.694564,
+      longitude : -73.718358
+  }
+
 ];
